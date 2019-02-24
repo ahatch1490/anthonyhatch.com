036369620972c355e63420872e5e614ef2448787
Anthony Hatch <anthonyhatch@Camila.local>
2019-02-23 16:19:30 -0800

    adding experience

--- anthonyhatch.com/src/main.ts
+++ anthonyhatch.com/src/main.ts
@@ -7,13 +7,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.css'
 import {library} from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faAddressCard, faBook} from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faAddressCard, faBook, faBuilding} from '@fortawesome/free-solid-svg-icons'
 import { faGithub,faLinkedin,faStackOverflow,faVuejs,faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 
 
-library.add(faCoffee,faBook,faAddressCard,faGithub,faLinkedin,faStackOverflow,faVuejs,faTwitter);
+library.add(faCoffee,faBook,faAddressCard,faGithub,faLinkedin,faStackOverflow,faVuejs,faTwitter,faBuilding);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
--- anthonyhatch.com/src/views/About.vue
+++ anthonyhatch.com/src/views/About.vue
@@ -1,6 +1,64 @@
 <template>
   <div class="about">
-    <h1></h1>
-    <p>Just a little project to play with Vuejs Bootstrap and Fontawesome</p>
+    <h1>Experience</h1>
+    <div class="row">
+      <div class="col-12">
+        <div class="card card_margin">
+          <div class="card-header fa-pull-left" style="background-color: white;" >
+            <h3>Moxi Works</h3>
+          </div>
+          <div class="card-body">
+            <img class="img-fluid rounded max_logo_height"  alt="Anthony Hatch" src="../assets/experience/MoxiWorks_logo.png"> <h3>2015 - 2019</h3>
+            <p>Sr. Software Engineer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card card_margin">
+          <div class="card-header fa-pull-left max_logo_height" style="background-color: white;" >
+            <h3>PayTrace</h3>
+          </div>
+          <div class="card-body">
+            <img class="img-fluid rounded max_logo_height"  alt="Anthony Hatch" src="../assets/experience/paytrace.svg"> <h3>2011 - 2015</h3>
+            <p>Sr. Software Engineer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card card_margin">
+          <div class="card-header fa-pull-left max_logo_height" style="background-color: white;" >
+            <h3>Study Manager</h3>
+          </div>
+          <div class="card-body" >
+            <font-awesome-icon icon="building" size="3x" />  <h3>2008 - 2011</h3>           <p>Sr. Software Engineer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card card_margin">
+          <div class="card-header fa-pull-left" style="background-color: white;" >
+            <h3>Education</h3>
+          </div>
+          <div class="card-body">
+            <img class="img-fluid rounded max_logo_height"  alt="Anthony Hatch" src="../assets/experience/westernlogo_med.png">
+            <h3>2004</h3>
+            <p>Degree: Bachelor Science, Computer Science.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+h3 {
+  margin-top: 5px;
+}
+
+</style>
