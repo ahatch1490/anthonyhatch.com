99f780c829cd27f90b7b36e65871a30b6005df2e
Anthony Hatch <tony@specmade.com>
2026-07-12 16:40:06 -0700

    feat: add Modern Software Engineering to books list

--- anthonyhatch.com/src/components/Books.vue
+++ anthonyhatch.com/src/components/Books.vue
@@ -44,6 +44,7 @@ const books: Book[] = [
     { title: 'The Pragmatic Programmer: From Journeyman to Master', cover: 'https://images.gr-assets.com/books/1401432508m/4099.jpg', url: 'https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer' },
     { title: 'A Philosophy of Software Design', cover: 'https://images.gr-assets.com/books/1531857377m/39996759.jpg', url: 'https://www.goodreads.com/book/show/39996759-a-philosophy-of-software-design' },
     { title: "The Imposter's Handbook (The Imposter's Handbook, #1)", cover: 'https://images.gr-assets.com/books/1546356393m/31572054.jpg', url: 'https://www.goodreads.com/book/show/31572054-the-imposter-s-handbook' },
+    { title: 'Modern Software Engineering: Doing What Works to Build Better Software Faster', cover: 'https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1635823845i/59517038.jpg', url: 'https://www.goodreads.com/book/show/59517038-modern-software-engineering' },
 ]
 </script>
 
